31513253542bd81fb7e78f35ab4e5438ae2276b6
morixxfoxdata <morixx.datascience@gmail.com>
2025-01-24 17:23:25 +0900

    やり直し

--- src/utils/exp_utils.py
+++ src/utils/exp_utils.py
@@ -24,23 +24,30 @@ print("# =======================================================================
 
 
 # ==========================================================================
-def target_diff():
-    target = np.load(exp_target)["arr_0"]
+def load_data(target_path, collect_path):
+    target = np.load(target_path)["arr_0"]
+    collect = np.load(collect_path)["arr_0"]
+    Y_all = target_diff(target)
+    X_all = collect_diff_and_skip(collect)
+    return Y_all, X_all
+
+
+def target_diff(target):
+    # target = np.load(exp_target)["arr_0"]
     # print("Target Image Shape: ", target.shape)
     diff_target = target[::2, :] - target[1::2, :]
     # print("diff_target Shape:", diff_target.shape)
     return diff_target
 
 
-def collect_diff_and_skip():
-    collect = np.load(exp_collected)["arr_0"]
+def collect_diff_and_skip(collect):
+    # collect = np.load(exp_collected)["arr_0"]
     diff_collect = collect[::2, :] - collect[1::2, :]
     diff_and_skip_collect = diff_collect[:, ::2]
     return diff_and_skip_collect
     # print("Collect signal Shape:", collect.shape)
 
 
-diff_target = target_diff()
-diff_collect = collect_diff_and_skip()
-print("diff_target Shape:", diff_target.shape)
-print("diff_collect Shape:", diff_collect.shape)
+Y_all, X_all = load_data(exp_target, exp_collected)
+print("Y_all Shape:", Y_all.shape)
+print("diff_collect Shape:", X_all.shape)
